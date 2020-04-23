0d4c8a416bceb33745071e2294df1a24668e3ff9
amongash <amongash08@gmail.com>
2020-04-23 16:12:49 +0300

    Updated the item model

--- server/models/ItemModel.js
+++ server/models/ItemModel.js
@@ -4,9 +4,20 @@ const ItemSchema = mongoose.Schema(
   {
     type: {
       type: String,
+      required: true,
+      min: 5,
+      max: 12,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
     },
   },
-  { timestamps: false }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Item', ItemSchema);
