d6b13e13428e24a4fad4b192f1d2f30663332ca7
amongash <amongash08@gmail.com>
2020-04-23 16:12:25 +0300

    Added new routes to items

--- server/routes/items/index.js
+++ server/routes/items/index.js
@@ -1,34 +1,16 @@
 const express = require('express');
-const PaymentModel = require('../../models/PaymentModel');
+const items = require('../../controllers/items');
 
 const router = express.Router();
 
-function redirectIfNotLoggedIn(req, res, next) {
-  if (!req.user) return res.redirect('/users');
-  return next();
-}
+// eslint-disable-next-line no-unused-vars
+module.exports = params => {
+  router.get('/', items.index);
+  router.post('/create', items.create);
 
-module.exports = () => {
-  // const { images } = params;
-
-  router.get('/', redirectIfNotLoggedIn, (req, res) => {
-    res.status(200).send('payment');
-  });
-
-  router.post('/', redirectIfNotLoggedIn, async (req, res, next) => {
-    try {
-      const payment = new PaymentModel({
-        amountPaid: req.body.amountPaid,
-      });
-
-      const savedPayment = await payment.save();
-
-      if (savedPayment) return res.json('Success: true');
-      return next(new Error('Failed to save payment for unknown reasons'));
-    } catch (err) {
-      return next(err);
-    }
-  });
+  router.get('/:itemId', items.findById);
+  router.put('/:itemId', items.update);
+  router.delete('/:itemId', items.destroy);
 
   return router;
 };
