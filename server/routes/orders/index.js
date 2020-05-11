b33e528ec72db545b2d0be17d0053b3bf4f404d5
amongash <amongash08@gmail.com>
2020-05-11 18:10:31 +0300

    Updated order routes

--- server/routes/orders/index.js
+++ server/routes/orders/index.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const OrderModel = require('../../models/OrderModel');
-const OrderItemModel = require('../../models/OrderItemModel');
-const OrderStatusModel = require('../../models/OrderStatusModel');
+const orders = require('../../controllers/orders');
 
 const router = express.Router();
 
 module.exports = () => {
   // const { images } = params;
 
-  router.get('/', (req, res) => {
-    res.status(200).send('payment');
-  });
+  router.get('/', orders.index);
+  router.post('/create', orders.addNewOrder);
 
-  router.post('/', async (req, res, next) => {
-    try {
-      // const payment = new PaymentModel({
-      //   amountPaid: req.body.amountPaid,
-      // });
-      // const savedPayment = await payment.save();
-      // if (savedPayment) return res.json('Success: true');
-      // return next(new Error('Failed to save payment for unknown reasons'));
-    } catch (err) {
-      return next(err);
-    }
-  });
+  router.get('/:orderId', orders.getById);
+  router.put('/:orderId', orders.update);
+  router.delete('/:orderId', orders.delete);
 
   return router;
 };
