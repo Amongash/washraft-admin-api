367ae8f2338b0640d7d70bf62efe3f0ca54cc654
amongash <amongash08@gmail.com>
2020-09-24 12:43:37 +0300

    Cleanup response

--- server/routes/home/index.js
+++ server/routes/home/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // eslint-disable-next-line no-unused-vars
 module.exports = params => {
   router.get('/', (req, res) => {
-    return res.json({ data: 'You are home', errors: [] });
+    return res.json({ data: 'You are home.' });
   });
   return router;
 };
